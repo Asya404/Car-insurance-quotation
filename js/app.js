eeba9212125de775852842c7ba7736f2babddbd8
Asya404 <unkiplum@gmail.com>
2022-03-01 14:44:32 +0100

    reading the values; error message

--- js/app.js
+++ js/app.js
@@ -1,16 +1,42 @@
 // Variables
+const form = document.querySelector('#request-quote');
+const html = new HTMLUI();
+
 
 
 
 // Event Listeners
-document.addEventListener('DOMContentLoaded', function () {
-    // create the option for the years
-    const html = new HTMLUI();
-    html.displayYears();
-});
+eventListeners();
+function eventListeners() {
+
+    // Create the option for the years
+    document.addEventListener('DOMContentLoaded', function () {
+        html.displayYears();
+    });
 
 
-// Dissplay years
+    // When the form is submitted
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        // Read the values from the form
+        const make = document.querySelector('#make').value;
+        const year = document.querySelector('#year').value;
+        const level = document.querySelector('input[name="level"]:checked').value;
+
+
+        if (make === '' || year === '' || level === '') {
+            html.displayError('All the fields are mandatory');
+        } else {
+            console.log('ok');
+        }
+    });
+}
+
+
+
+
+// Display yhe latest 20 years in the select
 function HTMLUI() { };
 
 HTMLUI.prototype.displayYears = function () {
@@ -25,4 +51,21 @@ HTMLUI.prototype.displayYears = function () {
         option.textContent = i;
         selectYears.appendChild(option);
     }
+}
+
+
+
+// Prints the error
+HTMLUI.prototype.displayError = function(message) {
+    const div = document.createElement('div');
+    div.classList.add('error');
+    div.innerHTML = `
+    <p>${message}</p>
+    `;
+    form.insertBefore(div, document.querySelector('.form-group'));
+
+    // Remove the error
+    setTimeout(function() {
+        document.querySelector('.error').remove();
+    }, 3000)
 }
